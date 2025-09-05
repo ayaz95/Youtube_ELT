2f86538559cbfc0e46cdc209024ca8d708f4f334
Ayaz Mujawar <ayazmujawar@Ayazs-MacBook-Air.local>
2025-09-05 20:29:32 +1000

    added get_video_ids function to get the full list of video ids

--- video_stats.py
+++ video_stats.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 def get_playlistID():
-
     try:
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
         response = requests.get(url)
@@ -22,6 +22,42 @@ def get_playlistID():
     except requests.exceptions.RequestException as e:
         raise e
 
+#playlistID = get_playlistID()
+
+def get_video_ids(playlistID):
+    
+    video_ids = []
+    pageToken = None
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistID}&key={API_KEY}"
+
+    try:
+
+        while True:
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
 if __name__ == "__main__":
-    get_playlistID()
+    playlistID = get_playlistID()
+    get_video_ids(playlistID)
 
